a8cdc09acd4dea42f0c0d399fd0c9966e9c81dd2
marilynakinyioyoo <marilyn.oyoo@student.moringaschool.com>
2024-06-29 20:19:21 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+    if (feet <= 400) {
+      return 'This one is on me!';
+    // Write your code here!
+  }
+    else if (feet <= 2000) {
+      return 'That will be twenty bucks.';
+    // Write your code here!
+  }
+    else if (feet >2000 && feet <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    // Write your code here!
+  }
+    else {
+      return 'No can do.';
+    // Write your code here!
+  }
+  }
+  function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
+  
+  function switchOnCharmFromTip(tip) {
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
+  }
